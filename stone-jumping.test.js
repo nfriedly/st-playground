67889eefc56b1b8a82e790104ce97fed56ab2111
Nathan Friedly <nathan@nfriedly.com>
2019-09-21 12:49:53 -0400

    fixed bug with high surface time, selected a different method with faster overall evaluation time

--- stone-jumping.test.js
+++ stone-jumping.test.js
@@ -1,7 +1,9 @@
 const assert = require("assert");
 
+const MAX_STONE_TIME = 100000;
+
 const bruteForce = (stones, maxJump) => {
-  for (let time = 0; time <= stones.length; time++) {
+  for (let time = 0; time <= MAX_STONE_TIME; time++) {
     let cur = -1;
     for (let i = 0; i < stones.length; i++) {
       const stoneTime = stones[i];
@@ -17,12 +19,65 @@ const bruteForce = (stones, maxJump) => {
       return time;
     }
   }
+  return -1;
 };
 
-// fastest
+// go from -1 to N in with lowest values in between
+// can jump up to D values
+// only positive values count
+// there are no duplicate positive values
+// if not possible, it should return -1
+
+// faster than rememberCur, but also known to be wrong on some inputs (!)
+// const badFirstPass = (A, D) => {
+//   let curPos = -1;
+//   for (let time = 0; time <= stones.length; time++) {
+//     // on each tick, test the stones within jumping range
+//     for (
+//       let stonePos = curPos + 1;
+//       stonePos < A.length && stonePos <= curPos + D;
+//       stonePos++
+//     ) {
+//       const aboveWaterTime = A[stonePos];
+//       const aboveWater = aboveWaterTime !== -1 && aboveWaterTime <= time;
+//       if (aboveWater) {
+//         curPos = stonePos;
+//       }
+//     }
+//     if (curPos + D >= A.length) {
+//       return time;
+//     }
+//   }
+//   return -1;
+// };
+
+// faster than rememberCur, but also known to be wrong on some inputs (!)
+const fixedFirstPass = (A, D) => {
+  let curPos = -1;
+  for (let time = 0; time <= MAX_STONE_TIME; time++) {
+    // on each tick, test the stones within jumping range
+    for (
+      let stonePos = curPos + 1;
+      stonePos < A.length && stonePos <= curPos + D;
+      stonePos++
+    ) {
+      const aboveWaterTime = A[stonePos];
+      const aboveWater = aboveWaterTime !== -1 && aboveWaterTime <= time;
+      if (aboveWater) {
+        curPos = stonePos;
+      }
+    }
+    if (curPos + D >= A.length) {
+      return time;
+    }
+  }
+  return -1;
+};
+
+// previous fastest overall, but slow when a stone has a very high surface time
 const rememberCur = (stones, maxJump) => {
   let cur = -1;
-  for (let time = 0; time <= stones.length; time++) {
+  for (let time = 0; time <= MAX_STONE_TIME; time++) {
     for (let i = cur; i < stones.length; i++) {
       if (cur + maxJump < i) {
         break;
@@ -36,11 +91,12 @@ const rememberCur = (stones, maxJump) => {
       return time;
     }
   }
+  return -1;
 };
 
 const optimized2 = (stones, maxJump) => {
   let cur = -1;
-  for (let time = 0; time <= stones.length; time++) {
+  for (let time = 0; time <= MAX_STONE_TIME; time++) {
     for (let i = cur; cur + maxJump >= i && i < stones.length; i++) {
       const stoneTime = stones[i];
       if (stoneTime > -1 && stoneTime <= time) {
@@ -51,12 +107,13 @@ const optimized2 = (stones, maxJump) => {
       return time;
     }
   }
+  return -1;
 };
 
 const optimized3 = (stones, maxJump) => {
   let cur = -1;
   const endZone = stones.length - maxJump;
-  for (let time = 0; time <= stones.length; time++) {
+  for (let time = 0; time <= MAX_STONE_TIME; time++) {
     for (let i = cur; cur + maxJump >= i && i < stones.length; i++) {
       const stoneTime = stones[i];
       if (stoneTime > -1 && stoneTime <= time) {
@@ -67,6 +124,7 @@ const optimized3 = (stones, maxJump) => {
       return time;
     }
   }
+  return -1;
 };
 
 const tooClever = (stones, maxJump) => {
@@ -74,16 +132,20 @@ const tooClever = (stones, maxJump) => {
   let timeRequired = 0;
   const endZone = stones.length - maxJump;
   while (position < endZone) {
-    const stonesToEvaluate = stones.slice(position + 1, position + 1 + maxJump);
+    const stonesToEvaluate = stones
+      .slice(position + 1, position + 1 + maxJump)
+      .filter(stoneTime => stoneTime > -1);
     //console.log({ stonesToEvaluate });
-    const nextStoneTime = stonesToEvaluate
-      .filter(stoneTime => stoneTime > -1)
-      .reduce((lowest, cur) => Math.min(lowest, cur));
+    if (!stonesToEvaluate.length) {
+      return -1;
+    }
+    const nextStoneTime = stonesToEvaluate.reduce((lowest, cur) =>
+      Math.min(lowest, cur)
+    );
     const nextPosition = stones.indexOf(nextStoneTime);
     assert(nextPosition > position, `${nextPosition} > ${position}`);
     position = nextPosition;
     timeRequired = Math.max(timeRequired, nextStoneTime);
-    //console.log({ position, timeRequired });
   }
   return timeRequired;
 };
@@ -94,11 +156,14 @@ const noLambda = (stones, maxJump) => {
   let timeRequired = 0;
   const endZone = stones.length - maxJump;
   while (position < endZone) {
-    const stonesToEvaluate = stones.slice(position + 1, position + 1 + maxJump);
+    const stonesToEvaluate = stones
+      .slice(position + 1, position + 1 + maxJump)
+      .filter(stoneTime => stoneTime > -1);
     //console.log({ stonesToEvaluate });
-    const nextStoneTime = stonesToEvaluate
-      .filter(stoneTime => stoneTime > -1)
-      .reduce(lowestReducer);
+    if (!stonesToEvaluate.length) {
+      return -1;
+    }
+    const nextStoneTime = stonesToEvaluate.reduce(lowestReducer);
     const nextPosition = stones.indexOf(nextStoneTime);
     assert(nextPosition > position, `${nextPosition} > ${position}`);
     position = nextPosition;
@@ -108,22 +173,26 @@ const noLambda = (stones, maxJump) => {
   return timeRequired;
 };
 
-// second fastest overall. Slightly on some inputs
-const lessClever = (stones, maxJump) => {
+// new fastest overall
+const lessClever = (A, D) => {
   let position = -1;
   let timeRequired = 0;
-  const endZone = stones.length - maxJump;
+  const endZone = A.length - D;
   while (position < endZone) {
     let nextStone = -1;
     let nextStoneTime = Infinity;
-    const curFarthestReachableStone = position + 1 + maxJump;
+    const curFarthestReachableStone = position + D;
     // for the stones within jumping distance, find the one that will surface first
-    for (let i = position + 1; i < curFarthestReachableStone; i++) {
-      const stoneTime = stones[i];
+    for (let i = position + 1; i <= curFarthestReachableStone; i++) {
+      const stoneTime = A[i];
       if (stoneTime > -1 && stoneTime <= nextStoneTime) {
         nextStoneTime = stoneTime;
         nextStone = i;
       }
+    }
+    if (nextStone === -1) {
+      // no stone within jumping distance will ever surface
+      return -1;
     }
     // jump to that stone and increase the time required if necessary
     position = nextStone;
@@ -134,30 +203,39 @@ const lessClever = (stones, maxJump) => {
 
 const stones = require("./10k-stones.json");
 describe.each([
-  ["brute force", bruteForce],
-  ["rememberCur", rememberCur],
-  ["optimized2", optimized2],
-  ["optimized3", optimized3],
-  ["too clever", tooClever],
-  ["noLambda", noLambda],
+  //["brute force", bruteForce],
+  //["bad first pass", badFirstPass],
+  ["fixed first pass", fixedFirstPass],
+  //["rememberCur", rememberCur],
+  // ["optimized2", optimized2],
+  // ["optimized3", optimized3],
+  //["too clever", tooClever],
+  //["noLambda", noLambda],
   ["less clever", lessClever]
 ])("%s", (name, calcTime) => {
   test.each([
     // stones, maxJump, expected
-    [[-1], 2, 0],
-    [[3, 2, 1], 1, 3],
-    [[-1, 1, -1], 2, 1],
-    [[-1, 6, -1, 5, -1, 4], 2, 6],
-    [[-1, 4, -1, 5, -1, 6], 2, 6],
-    [[-1, 4, -1, 5, 6, -1], 2, 6],
-    [[-1, 4, -1, 5, -1], 2, 5],
-    [[-1, 3, 4, -1, -1, 2], 3, 4],
-    [[7, 3, 10, 2, 4, 5, 9, 8, 6, 1], 1, 10],
-    [[7, 3, 10, 2, 4, 5, 9, 8, 6, 1], 2, 8],
-    [[7, 3, 10, 2, 4, 5, 9, 8, 6, 1], 3, 6],
-    [[7, 3, 10, 2, 4, 5, 9, 8, 6, 1], 4, 5],
-    [[7, 3, 10, 2, 4, 5, 9, 8, 6, 1], 5, 4],
-    [[7, 3, 10, 2, 4, 5, 9, 8, 6, 1], 6, 2]
+    // [[-1], 2, 0],
+    // [[-1, -1, -1, -1], 1, -1],
+    // [[-1, -1, -1, -1], 2, -1],
+    // [[-1, -1, -1, -1], 3, -1],
+    // [[-1, -1, -1, -1], 4, -1],
+    // [[-1, -1, -1, -1], 5, 0],
+    [[100000], 1, 100000],
+    [[-1, 100000, -1], 2, 100000]
+    // ,[[3, 2, 1], 1, 3],
+    // [[-1, 1, -1], 2, 1],
+    // [[-1, 6, -1, 5, -1, 4], 2, 6],
+    // [[-1, 4, -1, 5, -1, 6], 2, 6],
+    // [[-1, 4, -1, 5, 6, -1], 2, 6],
+    // [[-1, 4, -1, 5, -1], 2, 5],
+    // [[-1, 3, 4, -1, -1, 2], 3, 4],
+    // [[7, 3, 10, 2, 4, 5, 9, 8, 6, 1], 1, 10],
+    // [[7, 3, 10, 2, 4, 5, 9, 8, 6, 1], 2, 8],
+    // [[7, 3, 10, 2, 4, 5, 9, 8, 6, 1], 3, 6],
+    // [[7, 3, 10, 2, 4, 5, 9, 8, 6, 1], 4, 5],
+    // [[7, 3, 10, 2, 4, 5, 9, 8, 6, 1], 5, 4],
+    // [[7, 3, 10, 2, 4, 5, 9, 8, 6, 1], 6, 2]
   ])("Stones: %p maxJump: %i expected: %i", (stones, maxJump, expected) => {
     const actual = calcTime(stones, maxJump);
     expect(actual).toBe(expected);
@@ -181,5 +259,19 @@ describe.each([
   ])("10k array, maxJump: %i; expected: %i", (maxJumps, expected) => {
     const actual = calcTime(stones, maxJumps);
     expect(actual).toBe(expected);
+  });
+
+  test("100k array, 100k+1 jump", () => {
+    const arr = new Array(100000);
+    arr.fill(-1);
+    const jumpLength = arr.length + 1;
+    expect(calcTime(arr, jumpLength)).toBe(0);
+  });
+
+  test("100k array, 100k-1 jump", () => {
+    const arr = new Array(100000);
+    arr.fill(-1);
+    const jumpLength = arr.length - 1;
+    expect(calcTime(arr, jumpLength)).toBe(-1);
   });
 });
